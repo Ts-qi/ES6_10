fb3b7ded85edefdd413d70e552294366a4197cbf
qi.tang <qi.tang@weimob.com>
2020-04-14 22:38:47 +0800

    利用 generator ; 自定义得带器 Symbol.iterator

--- Iterator.js
+++ Iterator.js
@@ -15,16 +15,10 @@ let authors = {
 
 // }
 
-/** 
- * itertor 可遍历的接口
- * 
- * 
- * 
- */
-// 自定义遍历器 使其具有 for of 的迭代能力
-// 一：可迭代协议    二 ： 迭代器协议
+
 /**
- * 
+ * itertor 可遍历的接口
+ *  一：可迭代协议    二 ： 迭代器协议
  * 可迭代协议: 查找对象上是否有 Symbol.iterator； 如果有 则可以进行 for  of 的迭代
  * 迭代器协议: 必须返回 一个   
  * 			return {
@@ -37,29 +31,49 @@ let authors = {
  * 			}; 
  * 
  */
-authors[Symbol.iterator] = function()  {
-	let  allAuthors = this.allAuthors;// 拿到当前对象的allauthors
-	let  keys = Reflect.ownKeys(allAuthors)// ??  不知道作用
-	let  values = [];
-	return {
-		next(){
-			if(!values.length) {
-				if(keys.length) {
-					values = allAuthors[keys[0]]
-					keys.shift()// 剔除第一个
-				}
+//自定义遍历器 使其具有 for of 的迭代能力
+// authors[Symbol.iterator] = function()  {
+// 	let  allAuthors = this.allAuthors;// 拿到当前对象的allauthors
+// 	let  keys = Reflect.ownKeys(allAuthors)// ??  不知道作用
+// 	let  values = [];
+// 	return {
+// 		next(){
+// 			if(!values.length) {
+// 				if(keys.length) {
+// 					values = allAuthors[keys[0]]
+// 					keys.shift()// 剔除第一个
+// 				}
+// 			}
+// 			return {
+// 				done :!values.length,
+// 				value: values.shift()
+// 			}
+// 		}
+// 	}
+// }
+// 利用 generatator 实现  代码 更加简洁
+authors[Symbol.iterator] = function *() {
+	let allAuthors = this.allAuthors;
+	let keys = Reflect.ownKeys(allAuthors);
+	let values = []
+	while(1) { // 无限循环
+		if( !values.length ) {
+			if(keys.length) {
+				values = allAuthors[keys[0]];
+				keys.shift();// 剔除 对象的 key 的第一个值 
+				yield values.shift()//剔除 对象的  value 的第一个值
+			}else {
+				return false;//退出循环
 			}
-			return {
-				done :!values.length,
-				value: values.shift()
-			}
+		}else {
+			yield values.shift()
 		}
 	}
 }
-
+// 在次使用  for  of  就可以了
 let r = []
 for (let v of authors) {
 	r.push(v)
 }
-console.log(r)
+console.log(r) // 合并了 数组
 
