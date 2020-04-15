90ca8fd37865dd7976776d2f46c3555856b0a502
qi.tang <qi.tang@weimob.com>
2020-04-15 21:51:00 +0800

    Class

---
+++ Class.js
@@ -0,0 +1,18 @@
+
+
+// 以前 
+function Animal (name,type) {
+	this.name = name 
+	this.type = type
+}
+// es6
+class Animal {
+
+}
+
+// 私有  不能被访问的；  private  
+// 公用的 ，外部可以访问  public
+// 保护 class 内部属性 protected 
+// readonly class 内部属性；只读模式；
+
+//如何 继承？ 
